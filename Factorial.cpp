519fe4440281a6cc14e76894abc36771312fe574
BhuvanBose <68459580+bhuvantca@users.noreply.github.com>
2024-02-09 13:26:47 +0530

    Update Factorial.cpp

--- Factorial.cpp
+++ Factorial.cpp
@@ -14,3 +14,21 @@ int main() {
     std::cout<<fact;
     return 0;
 }
+
+//recursion
+#include <iostream>
+int fact(int n)
+{
+    if(n==0 || n==1)
+    return 1;
+    else
+    return n*fact(n-1);
+}
+
+int main() {
+    int number;
+    std::cout<<"Enter Number:";
+    std::cin>>number;
+    std::cout<<fact(number);
+    return 0;
+}
